c84e57ef95235bfc9e6b98ccd825053de9db1026
Oliver <os@ventose.cc>
2020-04-26 17:37:48 +0200

    .

---
+++ audiobackend/cmake-build-debug/CMakeFiles/audiobackend.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/audiobackend.dir/main.cpp.o"
+  "audiobackend"
+  "audiobackend.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/audiobackend.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
