c1c21238ccfa07ad143b97372367adc4f4edabae
Oliver <os@ventose.cc>
2020-04-30 16:46:08 +0200

    cleanup

--- Route-Planning/cmake-build-debug/thirdparty/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/parallels/CppGithub/Route-Planning/thirdparty/googletest
-# Build directory: /home/parallels/CppGithub/Route-Planning/cmake-build-debug/thirdparty/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
