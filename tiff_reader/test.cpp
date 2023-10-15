57abd9f3c4140685a2cf6b74fd2f84a0ae1a781e
Oliver Siegemund <47053492+Komplementariteten@users.noreply.github.com>
2023-10-15 15:42:00 +0200

    abc

--- tiff_reader/test.cpp
+++ tiff_reader/test.cpp
@@ -1,3 +1,18 @@
 //
 // Created by me on 14.10.23.
 //
+
+#include <catch2/catch_test_macros.hpp>
+struct IfdEntry {
+    uint16_t tag;
+    uint16_t field_type;
+    uint32_t count;
+    uint32_t value;
+};
+
+TEST_CASE("sizeof", "[sizeof]") {
+    REQUIRE(sizeof(short int) == 2);
+    REQUIRE(sizeof(std::uint16_t) == 2);
+    REQUIRE(sizeof(std::int16_t) == 2);
+    REQUIRE(sizeof(struct IfdEntry) == 12);
+}
